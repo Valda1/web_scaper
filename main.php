62bba10810f21f54afd24789ad1cf1df9770e9f7
Valda <valda.bimbirule1@gmail.com>
2024-08-03 16:56:24 +0300

    Initial commit

---
+++ main.php
@@ -0,0 +1,14 @@
+<?php
+
+#Initializing new cURL session; returns cURL handle
+$curl = curl_init();
+
+curl_setopt($curl, CURLOPT_URL, "http://example.com");
+
+#Returns result as a string instead of directly outputting it
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+#Fetches the content from URL and stores it in the $output variable as a string
+$output = curl_exec($curl);
+
+curl_close($curl);
